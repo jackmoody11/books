1af7065929a70e3b1966919e7a2f70568405ffd8
jackmoody11 <jackmoody@unc.edu>
2017-10-09 16:34:20 -0400

    Add gravatar for listings index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
     image_tag(gravatar_url, alt: user.username, class: "img-circle")
   end
 
+  def small_gravatar_for(user, options={size: 40})
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "avatar img-raised img-circle")
+  end
+
 end
