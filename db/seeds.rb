f33a8c452b2163243a0271e13eb8feca0ab87c7f
jackmoody11 <jackmoody@unc.edu>
2017-10-27 13:23:49 -0400

    Add seed info

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+admin = User.create(first_name: "Jack",
+                    last_name: "Moody",
+                    admin: true,
+                    email: "jacklm@live.unc.edu",
+                    password: "password",
+                    password_confirmation: "password")
+admin.skip_confirmation!
+admin.save!
