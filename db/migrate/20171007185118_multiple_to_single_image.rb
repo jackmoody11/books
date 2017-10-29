b816b6e6c669fc3f90948c6ff24f318348af1a64
jackmoody11 <jackmoody@unc.edu>
2017-10-29 17:19:39 -0400

    change from multiple to single image for heroku

--- db/migrate/20171007185118_multiple_to_single_image.rb
+++ db/migrate/20171007185118_multiple_to_single_image.rb
@@ -1,5 +1,8 @@
 class MultipleToSingleImageForHeroku < ActiveRecord::Migration[5.1]
   def change
     remove_column :listings, :images, :json
+    change_table :listings do |t|
+      t.string :image
+    end
   end
 end
