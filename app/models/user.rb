6d6f80fecdeae2eef9a061dd99c4c9b2d385d7cd
jackmoody11 <jackmoody@unc.edu>
2017-10-29 11:32:13 -0400

    Allow nil first and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :listings, dependent: :destroy
   has_many :categories, through: :listings
-  before_save { self.email = email.downcase, self.first_name = first_name.titleize, self.last_name = last_name.titleize}
+  before_save { self.email = email.downcase, self.first_name = first_name.titleize if !first_name.nil? , self.last_name = last_name.titleize if !first_name.nil?}
   VALID_UNC_EMAIL = /\A[\w+\-.]+@live.unc.edu\z/i
   validates :email, presence: true, length: { maximum: 105 },
                     uniqueness: { case_sensitive: false },
                     format: { with: VALID_UNC_EMAIL, message: "You must use a valid @live.unc.edu email" }
     current_year = Time.now.year
-    validates :first_name, length: {maximum: 20}
-    validates :last_name, length: {maximum: 20}
+    validates :first_name, length: {maximum: 20}, allow_nil: true
+    validates :last_name, length: {maximum: 20}, allow_nil: true
     # Optionals
     validates :summary, length: { maximum: 500}, allow_nil: true
     validates :grad_year, numericality: { greater_than_or_equal_to: current_year,
