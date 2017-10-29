b5f466e230f5fae7887c48a5191160d6f20cf41b
jackmoody11 <jackmoody@unc.edu>
2017-10-29 17:08:47 -0400

    Get rid of multiple images option in heroku

--- db/migrate/20171007185118_multiple_to_single_image.rb
+++ db/migrate/20171007185118_multiple_to_single_image.rb
@@ -1,4 +1,5 @@
-class MultipleToSingleImage < ActiveRecord::Migration[5.1]
+class MultipleToSingleImageForHeroku < ActiveRecord::Migration[5.1]
   def change
+    remove_column :listings, :images, :json
   end
 end
--- db/migrate/20171029204936_add_images_to_heroku.rb
+++
@@ -1,7 +0,0 @@
-class AddImagesToHeroku < ActiveRecord::Migration[5.1]
-  def change
-    change_table :listings do |t|
-      t.string :image
-    end
-  end
-end
