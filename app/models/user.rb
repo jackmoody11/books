adb9fe61164ea7e65545f2ecbd99fd33a239bf35
jackmoody11 <jackmoody@unc.edu>
2017-10-29 16:28:17 -0400

    make sure all ends are closed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :listings, dependent: :destroy
   has_many :categories, through: :listings
-  before_save { self.email = email.downcase, self.first_name = first_name.titleize, self.last_name = last_name.titleize}
+  before_save { self.email = email.downcase, self.first_name = first_name.titleize, self.last_name = last_name.titleize }
   VALID_UNC_EMAIL = /\A[\w+\-.]+@live.unc.edu\z/i
   validates :email, presence: true, length: { maximum: 105 },
                     uniqueness: { case_sensitive: false },
                     format: { with: VALID_UNC_EMAIL, message: "You must use a valid @live.unc.edu email" }
   current_year = Time.now.year
-  validates :first_name, length: {maximum: 20}
-  validates :last_name, length: {maximum: 20}
+  validates :first_name, length: { maximum: 20 }
+  validates :last_name, length: { maximum: 20 }
     # Optionals
-    validates :summary, length: { maximum: 500}, allow_nil: true
+    validates :summary, length: { maximum: 500 }, allow_nil: true
     validates :grad_year, numericality: { greater_than_or_equal_to: current_year,
                                           less_than_or_equal_to: current_year + 4,
                                           message: "You must be graduating within the next 4 years" },
