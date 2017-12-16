33229234a5639156ff27775dd49b0ab23b81d668
jackmoody11 <jackmoody@unc.edu>
2017-12-16 14:56:35 -0500

    Simplify pages controller and add course addition to admin nav

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,27 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!
   def show
-    render template: "pages/#{params[:page]}"
+    render template: "#{params[:page]}"
   end
-
-  def home
-    render 'home'
-  end
-
-  def about
-    render 'about'
-  end
-
-  def contact
-    render 'contact'
-  end
-
-  def terms
-    render 'terms'
-  end
-
-  def privacy
-    render 'privacy'
-  end
-
 end
