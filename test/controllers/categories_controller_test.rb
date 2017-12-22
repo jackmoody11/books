0a15372449b7083e8c8068741e9dccdc042e8517
jackmoody11 <jackmoody@unc.edu>
2017-12-21 20:03:12 -0500

    Add new tests to categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -5,22 +5,28 @@ class CategoriesControllerTest < ActionController::TestCase
     sign_in users(:one)
   end
 
-  test "test_the_truth" do
-    assert true
-  end
-
-  test "should get categories index" do
+  test "admin can get index categories page" do
     get :index
     assert_response :success
   end
 
-  test "should get new" do
+  test "admin can get new categories page" do
     get :new
     assert_response :success
   end
 
-# Need to sign out admin and sign_in users(:two)
-  # test "should redirect create when admin not logged in" do
+  test "category does not save without name" do
+    category = Category.new(shortname: "ABCD")
+    assert_not category.save
+  end
 
-  # end
+  test "category does not save without shortname" do
+    category = Category.new(name: "ABCD Class")
+    assert_not category.save
+  end
+
+  test "proper category saves" do
+    category = Category.new(name: "ABCD Class", shortname: "ABCD")
+    assert category.save
+  end
 end
