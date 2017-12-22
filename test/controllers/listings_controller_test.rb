b91987b2c6e9ce96db88ab95b164dbdcedf09fb9
jackmoody11 <jackmoody@unc.edu>
2017-12-21 20:24:08 -0500

    Begin creating own tests

--- test/controllers/listings_controller_test.rb
+++ test/controllers/listings_controller_test.rb
@@ -2,8 +2,7 @@ require 'test_helper'
 
 class ListingsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::ControllerHelpers
-  setup do
-    @listing = listings(:one)
-  end
+
+  
 
 end
--- test/models/listing_test.rb
+++ test/models/listing_test.rb
@@ -1,4 +1,16 @@
 require 'test_helper'
 
 class ListingTest < ActiveSupport::TestCase
+
+  # test "successfully create listing" do
+  #   visit new_listings_path
+  #
+  #   fill_in "Title of book", with: "Adding a new test book"
+  #   fill_in "CONDITION", with: "New"
+  #   fill_in "Price (in $USD)", with: 123
+  #
+  #   click_on "Create Listing"
+  #   assert_text "Adding a new test book"
+  # end
+
 end
