6447a36c1725fceab683b3865e4051a879d29890
jackmoody11 <jackmoody@unc.edu>
2018-01-02 14:41:06 -0500

    Add helper methods and expand on conditional views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,5 +12,31 @@ module ApplicationHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "avatar img-raised img-circle")
   end
+  # Name existence helpers
+  def show_first_name(listing)
+    if listing.user.first_name
+      listing.user.first_name
+    end
+  end
+
+  def show_last_name(listing)
+    if listing.user.last_name
+      listing.user.last_name
+    end
+  end
+
+  def show_full_name(listing)
+    if listing.user.first_name && listing.user.last_name
+      listing.user.first_name + ' ' + listing.user.last_name
+    end
+  end
+
+  # Author existence
+
+  def show_author(listing)
+    if listing.author
+      truncate(listing.author, length: 25)
+    end
+  end
 
 end
